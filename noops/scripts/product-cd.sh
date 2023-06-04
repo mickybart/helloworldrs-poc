ebcce71f8506afd6fa73a167175685efda1eec70
mickybart <mickybart@pygoscelis.org>
2023-06-04 12:31:11 -0400

    noops: minimal poc

---
+++ noops/scripts/product-cd.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+function usage {
+    cat << EOF 1>&2
+
+Usage: $(basename $0)
+
+Run continuous delivery for product.
+
+The product must be a git repository.
+Commit will be used to set the product version.
+
+Environement variables expected:
+--------------------------------
+REPOSITORY            The repository name
+
+EOF
+    exit 1
+}
+
+echo "Running CD..."
+
+if [ -z "$REPOSITORY" ]; then
+    usage
+fi
+
+source /builder/modules/oci-registry-init
+
+VERSION=sha-$(git rev-parse --short=7 HEAD)
+TAG=$(registry_image_tag $REPOSITORY $VERSION)
+DOCKERFILE=$(cat $NOOPS_GENERATED_JSON | jq -r .package.docker.product.dockerfile)
+
+docker buildx build \
+    --tag $TAG \
+    -f $DOCKERFILE .
+
+docker push $TAG
+
+echo "CD done with success"
